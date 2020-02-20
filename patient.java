1e9146c7b3ef80ef4a7ce01c184008507447414b
Garron David Blake <49132552+WeebHunter69@users.noreply.github.com>
2020-02-20 11:25:21 -0500

    Add files via upload

---
+++ patient.java
@@ -0,0 +1,37 @@
+
+public class patient extends personnel
+{
+	private int patientID;
+	private String firstName;
+	private String middleInitial;
+	private String lastName;
+	private String birthMonth;
+	private String birthYear;
+	private String birthDay;
+	private String sex;
+	private String streetAddress;
+	private String city;
+	private String state;
+	private String zipCode;
+	private String workPhoneNumber;
+	private String homePhoneNumber;
+	private String socialSecurityNumber;
+	private String employerName;
+	private String employerStreetAddress;
+	private String employerCity;
+	private String employerState;
+	private String employerZipCode;
+	private String responsibleName;
+	private String responsibleRelationship;
+	private String responsibleStreetAddress;
+	private String responsibleCity;
+	private String responsibleState;
+	private String responsibleZipCode;
+	private String responsibleWorkPhoneNumber;
+	private String responsibleHomePhoneNumber;
+	private String responsibleSocialSecurityNumber;
+	
+		public patient (int patientID, String first_name, String middle_Initial)
+		{
+		}
+}
